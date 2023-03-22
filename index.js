d39151d8c5f4517e7ac47f69b4ba0e80ec8f48bc
yyll <lyyl6270@163.com>
2023-03-22 16:25:24 +0800

    feat:canvas

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const ding = require('./src/core/ding')
 const mini = require('./src/core/mini')
+const canvans = require('./lib/core/canvas')
 
 module.exports = {
     ding: ding,
     mini: mini,
+    canvans: canvans,
 };
