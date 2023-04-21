3a081dbda04d4e9c8062ea9db19d0c83f4b473eb
yyll <lyyl6270@163.com>
2023-04-21 13:13:52 +0800

    feat:add deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const ding = require('./src/core/ding')
-const mini = require('./src/core/mini')
-const canvas = require('./src/core/canvas')
-const oss = require('./src/core/oss')
+const ding = require("./src/core/ding");
+const mini = require("./src/core/mini");
+const canvas = require("./src/core/canvas");
+const oss = require("./src/core/oss");
+const deploy = require("./src/core/deploy");
 
 module.exports = {
-    ding: ding,
-    mini: mini,
-    canvas: canvas,
-    oss: oss,
+  ding: ding,
+  mini: mini,
+  canvas: canvas,
+  oss: oss,
+  deploy: deploy,
 };
---
+++ src/core/deploy.js
@@ -0,0 +1,5 @@
+const Deploy = require("../../lib/core/deploy");
+
+module.exports = function (options) {
+  return new Deploy(options);
+};
